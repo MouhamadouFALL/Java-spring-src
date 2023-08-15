19274dd1c2086e28a3385f62db8af5cf7e0c135c
Mouhamadou FALL <mouhamadou2.fall@gmail.com>
2023-08-15 11:48:46 +0000

    Conteneur léger Spring
    Appréhender les conteneurs légers de Spring. comprendre leur organisation et leur fonctionnement.

--- facture/src/main/java/com/siggidieuf/facture/App.java
+++ facture/src/main/java/com/siggidieuf/facture/App.java
@@ -10,6 +10,8 @@ import com.siggidieuf.facture.repository.InvoiceRepositoryDatabase;
 import com.siggidieuf.facture.service.IinvoiceService;
 import com.siggidieuf.facture.service.InvoiceServiceNumber;
 import com.siggidieuf.facture.service.InvoiceServicePrefix;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
@@ -21,45 +23,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your Controller name(Web, Keyboard, Douchet): ");
-        String controllerName = scan.nextLine();
-        controllerName = "com.siggidieuf.facture.controller.InvoiceController"+stringUpperFirst(controllerName);
-        System.out.println("Enter your Service name(Number, Prefix): ");
-        String serviceName = scan.nextLine();
-        serviceName = "com.siggidieuf.facture.service.InvoiceService"+stringUpperFirst(serviceName);
-        System.out.println("Enter your Repository name(Memory, Database): ");
-        String repositoryName = scan.nextLine();
-        repositoryName = "com.siggidieuf.facture.repository.InvoiceRepository"+stringUpperFirst(repositoryName);
-
-
-        IinvoiceController invoiceController = null;
-        IinvoiceService invoiceService = null;
-        IinvoiceRepository invoiceRepository = null;
-
-        try {
-            invoiceController = (IinvoiceController) Class.forName(controllerName).getDeclaredConstructor().newInstance();
-            invoiceService = (IinvoiceService) Class.forName(serviceName).getDeclaredConstructor().newInstance();
-            invoiceRepository = (IinvoiceRepository) Class.forName(repositoryName).getDeclaredConstructor().newInstance();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        invoiceController.setInvoiceService(invoiceService);
-        invoiceService.setInvoiceRepository(invoiceRepository);
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        IinvoiceController invoiceController = context.getBean(IinvoiceController.class);
         invoiceController.createInvoice();
 
-        if (scan != null) scan.close();
-
     }
 
-    private static String stringUpperFirst(String chaine){
-        String sousCaine = chaine.substring(1);
-        String caractUpper = String.valueOf(chaine.trim().toUpperCase().charAt(0));
-
-        return (caractUpper+sousCaine);
-
-    }
 }
